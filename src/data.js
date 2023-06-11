dd5cf66efe5b13f2e825a32ac32f93d0c67932c2
Robert Gleason <l2ob122@gmail.com>
2023-06-10 17:57:34 -0700

    made changes to services data

--- src/data.js
+++ src/data.js
@@ -10,3 +10,47 @@ export const socialLinks = [
   { id: 2, href: 'https://twitter.com', icon: 'fab fa-twitter' },
   { id: 2, href: 'https://twitter.com', icon: 'fab fa-squarespace' },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ex?',
+  },
+  {
+    id: 1,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ex?',
+  },
+  {
+    id: 1,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ex?',
+  },
+];
+
+const tours = [
+  {
+    id: 1,
+    img: './images/tour-1.jpeg',
+    date: 'august 26th 2023',
+    title: 'tibet adventure',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam saepe sed recusandae exercitationem. Iste, iure! Nobis aperiam ullam facilis atque.',
+    country: 'china',
+    days: '6 days',
+    price: 'from $2100',
+  },
+  {
+    id: 2,
+    img: './images/tour-2.jpeg',
+    date: 'august 26th 2023',
+    title: 'tibet adventure',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam saepe sed recusandae exercitationem. Iste, iure! Nobis aperiam ullam facilis atque.',
+    country: 'china',
+    days: '6 days',
+    price: 'from $2100',
+  },
+];
